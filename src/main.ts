43493c2a5f4640d66b317cebd63d2ff614e5baba
yoonshinaelim <ysal4505@gamil.com>
2026-04-09 16:18:49 +0900

    수정

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import './main.scss';
+
+const menuBtn = document.querySelector('#btn-menu') as HTMLButtonElement | null;
+const navList = document.querySelector('#nav-list') as HTMLElement | null;
+
+if (menuBtn && navList) {
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('on');
+    navList.classList.toggle('on');
+    
+    const isExpanded = menuBtn.classList.contains('on');
+    menuBtn.setAttribute('aria-expanded', isExpanded.toString());
+  });
+}
