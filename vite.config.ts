bd5d3e456597c7685dc88b1dae54b4ee29ad3b93
yoonshinaelim <ysal4505@gamil.com>
2026-04-10 12:15:19 +0900

    수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/p2026/', // 이 부분을 꼭 추가해야 경로 에러가 안 납니다!
+  base: '/p2026/', 
 })
