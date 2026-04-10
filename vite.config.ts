77dea9693cdc62724beb0a74cc1555f60811d973
yoonshinaelim <ysal4505@gamil.com>
2026-04-10 14:24:31 +0900

    수정

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // 1. 리액트 플러그인 가져오기
+
+export default defineConfig({
+  plugins: [react()], // 2. 플러그인 적용
+  base: '/p2026/',
+})
