67de3b4211619a8d0d33679cdd185a619cdb65ed
yoonshinaelim <ysal4505@gamil.com>
2026-04-10 14:41:01 +0900

    react 수정

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import './scss/main.scss';
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' 
+import './scss/main.scss' 
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
